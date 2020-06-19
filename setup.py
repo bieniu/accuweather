52429625b834c2251ed529459f676de4826248d5
Maciej Bieniek <maciej.bieniek@gmail.com>
2020-06-19 08:50:42 +0200

    Add setup files

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(
+    name="accuweather",
+    version="0.0.1",
+    author="Maciej Bieniek",
+    author_email="maciej.bieniek@gmail.com",
+    description="Python wrapper for getting weather data from AccuWeather servers.",
+    include_package_data=True,
+    url="https://github.com/bieniu/accuweather",
+    license="Apache 2",
+    packages=["accuweather"],
+    python_requires=">=3.6",
+    install_requires=["aiohttp"],
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+    ],
+    setup_requires=("pytest-runner"),
+    tests_require=(
+        "asynctest",
+        "pytest-cov",
+        "pytest-asyncio",
+        "pytest-trio",
+        "pytest-tornasync",
+    ),
+)
