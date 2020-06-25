c3d6f82b73a8f8bc5fa670b8d551a3b5d89a5bc4
Maciej Bieniek <bieniu@users.noreply.github.com>
2020-06-25 22:29:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="accuweather",
-    version="0.1.0",
+    version="0.0.6",
     author="Maciej Bieniek",
     author_email="maciej.bieniek@gmail.com",
     description="Python wrapper for getting weather data from AccuWeather servers.",
