6d4afbf431b8950e6adcd924b3c882ced7d727ae
Maciej Bieniek <bieniu@users.noreply.github.com>
2020-07-06 11:20:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+with open("README.md", "r") as file:
+    long_description = file.read()
+
 setup(
     name="accuweather",
     version="0.0.7",
     author="Maciej Bieniek",
     author_email="maciej.bieniek@gmail.com",
     description="Python wrapper for getting weather data from AccuWeather servers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     include_package_data=True,
     url="https://github.com/bieniu/accuweather",
     license="Apache 2",
-    packages=["accuweather"],
+    packages=setuptools.find_packages()
     python_requires=">=3.6",
     install_requires=["aiohttp"],
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
     setup_requires=("pytest-runner"),
