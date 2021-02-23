cf86e3b36ad0a7598e12092ab5adc7e62dead271
Maciej Bieniek <maciej.bieniek@gmail.com>
2021-02-23 10:10:51 +0100

    Use aioresponse for tests

--- setup.py
+++ setup.py
@@ -24,5 +24,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     setup_requires=("pytest-runner"),
-    tests_require=("pytest-cov", "pytest-asyncio", "pytest-error-for-skips"),
+    tests_require=("pytest-cov", "pytest-asyncio", "pytest-error-for-skips", "aioresponses"),
 )
--- tests/test_base.py
+++ tests/test_base.py
@@ -2,8 +2,10 @@
 import json
 from unittest.mock import patch
 
+import aiohttp
 import pytest
 from aiohttp import ClientSession
+from aioresponses import aioresponses
 
 from accuweather import (
     AccuWeather,
@@ -13,11 +15,12 @@ from accuweather import (
     RequestsExceededError,
 )
 
-VALID_API_KEY = "32-character-string-1234567890qw"
+HEADERS = {"RateLimit-Remaining": "23"}
 INVALID_API_KEY = "abcdef"
 LATITUDE = 52.0677904
-LONGITUDE = 19.4795644
 LOCATION_KEY = "268068"
+LONGITUDE = 19.4795644
+VALID_API_KEY = "32-character-string-1234567890qw"
 
 
 @pytest.mark.asyncio
@@ -26,15 +29,24 @@ async def test_get_location():
     with open("tests/data/location_data.json") as file:
         location_data = json.load(file)
 
-    with patch("accuweather.AccuWeather._async_get_data", return_value=location_data):
-        async with ClientSession() as websession:
-            accuweather = AccuWeather(
-                VALID_API_KEY, websession, latitude=LATITUDE, longitude=LONGITUDE
-            )
-            await accuweather.async_get_location()
+    session = aiohttp.ClientSession()
 
-        assert accuweather.location_name == "Piątek"
-        assert accuweather.location_key == "268068"
+    with aioresponses() as session_mock:
+        session_mock.get(
+            "https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=32-character-string-1234567890qw&q=52.0677904%252C19.4795644",
+            payload=location_data,
+            headers=HEADERS,
+        )
+        accuweather = AccuWeather(
+            VALID_API_KEY, session, latitude=LATITUDE, longitude=LONGITUDE
+        )
+        await accuweather.async_get_location()
+
+    await session.close()
+
+    assert accuweather.location_name == "Piątek"
+    assert accuweather.location_key == "268068"
+    assert accuweather.requests_remaining == "23"
 
 
 @pytest.mark.asyncio
@@ -43,23 +55,28 @@ async def test_get_current_conditions():
     with open("tests/data/current_condition_data.json") as file:
         current_condition_data = json.load(file)
 
-    with patch(
-        "accuweather.AccuWeather._async_get_data", return_value=current_condition_data
-    ):
-        async with ClientSession() as websession:
-            accuweather = AccuWeather(
-                VALID_API_KEY, websession, location_key=LOCATION_KEY
-            )
-            current_conditions = await accuweather.async_get_current_conditions()
+    session = aiohttp.ClientSession()
 
-        assert current_conditions["WeatherIcon"] == 7
-        assert isinstance(current_conditions["HasPrecipitation"], bool)
-        assert not current_conditions["HasPrecipitation"]
-        assert not current_conditions["PrecipitationType"]
-        assert current_conditions["Temperature"]["Metric"]["Value"] == 23.1
-        assert current_conditions["Temperature"]["Metric"]["Unit"] == "C"
-        assert current_conditions["Temperature"]["Imperial"]["Value"] == 74
-        assert current_conditions["Temperature"]["Imperial"]["Unit"] == "F"
+    with aioresponses() as session_mock:
+        session_mock.get(
+            "https://dataservice.accuweather.com/currentconditions/v1/268068?apikey=32-character-string-1234567890qw&details=true",
+            payload=current_condition_data,
+            headers=HEADERS,
+        )
+        accuweather = AccuWeather(VALID_API_KEY, session, location_key=LOCATION_KEY)
+        current_conditions = await accuweather.async_get_current_conditions()
+
+    await session.close()
+
+    assert current_conditions["WeatherIcon"] == 7
+    assert isinstance(current_conditions["HasPrecipitation"], bool)
+    assert not current_conditions["HasPrecipitation"]
+    assert not current_conditions["PrecipitationType"]
+    assert current_conditions["Temperature"]["Metric"]["Value"] == 23.1
+    assert current_conditions["Temperature"]["Metric"]["Unit"] == "C"
+    assert current_conditions["Temperature"]["Imperial"]["Value"] == 74
+    assert current_conditions["Temperature"]["Imperial"]["Unit"] == "F"
+    assert accuweather.requests_remaining == "23"
 
 
 @pytest.mark.asyncio
@@ -68,28 +85,36 @@ async def test_get_forecast():
     with open("tests/data/forecast_data.json") as file:
         forecast_data = json.load(file)
 
-    with patch("accuweather.AccuWeather._async_get_data", return_value=forecast_data):
-        async with ClientSession() as websession:
-            accuweather = AccuWeather(
-                VALID_API_KEY, websession, location_key=LOCATION_KEY
-            )
-            forecast = await accuweather.async_get_forecast()
+    session = aiohttp.ClientSession()
 
-        assert forecast[0]["IconDay"] == 15
-        assert forecast[0]["PrecipitationProbabilityDay"] == 57
-        assert forecast[0]["WindDay"]["Speed"]["Value"] == 13.0
-        assert forecast[0]["TemperatureMax"]["Value"] == 24.8
-        assert forecast[0]["TemperatureMax"]["Unit"] == "C"
-        assert forecast[0]["Ozone"]["Value"] == 23
+    with aioresponses() as session_mock:
+        session_mock.get(
+            "https://dataservice.accuweather.com/forecasts/v1/daily/5day/268068?apikey=32-character-string-1234567890qw&details=true&metric=True",
+            payload=forecast_data,
+            headers=HEADERS,
+        )
+
+        accuweather = AccuWeather(VALID_API_KEY, session, location_key=LOCATION_KEY)
+        forecast = await accuweather.async_get_forecast()
+
+    await session.close()
+
+    assert forecast[0]["IconDay"] == 15
+    assert forecast[0]["PrecipitationProbabilityDay"] == 57
+    assert forecast[0]["WindDay"]["Speed"]["Value"] == 13.0
+    assert forecast[0]["TemperatureMax"]["Value"] == 24.8
+    assert forecast[0]["TemperatureMax"]["Unit"] == "C"
+    assert forecast[0]["Ozone"]["Value"] == 23
+    assert accuweather.requests_remaining == "23"
 
 
 @pytest.mark.asyncio
 async def test_invalid_api_key():
     """Test with invalid API key."""
-    async with ClientSession() as websession:
+    async with ClientSession() as session:
         try:
             AccuWeather(
-                INVALID_API_KEY, websession, latitude=LATITUDE, longitude=LONGITUDE
+                INVALID_API_KEY, session, latitude=LATITUDE, longitude=LONGITUDE
             )
         except InvalidApiKeyError as error:
             assert (
@@ -101,9 +126,9 @@ async def test_invalid_api_key():
 @pytest.mark.asyncio
 async def test_invalid_coordinates_1():
     """Test with invalid coordinates."""
-    async with ClientSession() as websession:
+    async with ClientSession() as session:
         try:
-            AccuWeather(VALID_API_KEY, websession, latitude=55.55, longitude="78.00")
+            AccuWeather(VALID_API_KEY, session, latitude=55.55, longitude="78.00")
         except InvalidCoordinatesError as error:
             assert str(error.status) == "Your coordinates are invalid"
 
@@ -111,9 +136,9 @@ async def test_invalid_coordinates_1():
 @pytest.mark.asyncio
 async def test_invalid_coordinates_2():
     """Test with invalid coordinates."""
-    async with ClientSession() as websession:
+    async with ClientSession() as session:
         try:
-            AccuWeather(VALID_API_KEY, websession, latitude=199.99, longitude=90.0)
+            AccuWeather(VALID_API_KEY, session, latitude=199.99, longitude=90.0)
         except InvalidCoordinatesError as error:
             assert str(error.status) == "Your coordinates are invalid"
 
