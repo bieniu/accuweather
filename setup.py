a35c58fdedd81f375c2c2bf42a409e1badaf1556
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-02-23 12:44:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as file:
 
 setup(
     name="accuweather",
-    version="0.0.11",
+    version="0.1.0",
     author="Maciej Bieniek",
     author_email="maciej.bieniek@gmail.com",
     description="Python wrapper for getting weather data from AccuWeather servers.",
