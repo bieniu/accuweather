7136115f4826fa871635847840255a52d34d5b3b
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-03-24 10:57:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description_content_type="text/markdown",
     include_package_data=True,
     url="https://github.com/bieniu/accuweather",
-    license="Apache License 2.0",
+    license="Apache-2.0 License",
     packages=find_packages(exclude=["tests"]),
     python_requires=">=3.6",
     install_requires=list(val.strip() for val in open("requirements.txt")),
