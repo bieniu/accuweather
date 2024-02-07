40bcaad482705d764fe255973084828fb13c9a0d
Maciej Bieniek <bieniu@users.noreply.github.com>
2024-02-07 08:38:22 +0100

    Drop Python 3.10 support (#324)
    Co-authored-by: Maciej Bieniek <478555+bieniu@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     license="Apache-2.0 License",
     packages=["accuweather"],
     package_data={"accuweather": ["py.typed"]},
-    python_requires=">=3.10",
+    python_requires=">=3.11",
     install_requires=["aiohttp>=3.7.0", "orjson"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -27,7 +27,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3 :: Only",
