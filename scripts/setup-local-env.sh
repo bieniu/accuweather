46c54a33872b1f80584f8773623082e9f5ee4cd2
Maciej Bieniek <bieniu@users.noreply.github.com>
2025-04-16 16:27:17 +0200

    Move dependency list to pyproject.toml file (#550)
    * Remove requirements.txt
    
    * Remove requirements*.txt files

--- scripts/setup-local-env.sh
+++ scripts/setup-local-env.sh
@@ -6,5 +6,5 @@ python$PYTHON_VERSION -m pip install uv
 python$PYTHON_VERSION -m uv venv venv --seed --python=$PYTHON_VERSION
 source venv/bin/activate
 pip install uv
-uv pip install -r requirements-dev.txt
+uv pip install . .[test] .[dev]
 pre-commit install
